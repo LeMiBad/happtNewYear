823f1869255d5c5b478295c891e49918d9b82765
Leonid <79872121399.yandex@gmail.com>
2022-12-26 00:58:54 +0300

    first commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -34,7 +34,7 @@ const GlobalStyles = createGlobalStyle`
 
 const App = () => {
     const currentPage = useStore($currentPage)
-    const pages = [<WinPage/>, <MenuPage/>, <RulesPage/>, <RecipePage/>, <GamePage/>, <LoosePage/>, <WinPage/>, <FinalPage/>, <LastCarier/>, <Carier/>]
+    const pages = [<StartPage/>, <MenuPage/>, <RulesPage/>, <RecipePage/>, <GamePage/>, <LoosePage/>, <WinPage/>, <FinalPage/>, <LastCarier/>, <Carier/>]
 
 
     if(window.innerHeight < window.innerWidth) return <Desktop/>
