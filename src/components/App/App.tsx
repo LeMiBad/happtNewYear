129e764da57f57cbd12f84f84b22aa0153749995
Leonid <79872121399.yandex@gmail.com>
2022-12-25 22:29:48 +0300

    first commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -41,7 +41,7 @@ const App = () => {
 
 
     return (
-        <div style={{position: 'absolute', width: '100%', height: '100vh', overflow: "hidden"}}>
+        <div style={{position: 'absolute', width: '100%', height: '100%', overflow: "hidden"}}>
             <GlobalStyles/>
             {pages[currentPage]}
             <Kitchen/>
